f131e51a9278dbb8cbcd0621a72df72848020be3
Alejandro Bastidas <alejandro.bastidas@external.appspace.com>
2025-05-07 20:40:53 -0600

    add new endpoint to simulate heavier loads

--- kubernetes-app/Program.cs
+++ kubernetes-app/Program.cs
@@ -1,3 +1,5 @@
+using System.Numerics; // Necesario para BigInteger
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -19,9 +21,31 @@ app.MapGet("/test", async () =>
     Random random = new Random();
     int delay = random.Next(0, 1001); // Genera un retraso aleatorio entre 0 y 1000 ms
     await Task.Delay(delay);
-    
+
     string podName = Environment.GetEnvironmentVariable("HOSTNAME") ?? "unknown-pod";
-    return Results.Ok($"Ok from pod: {podName} - {Guid.NewGuid()}");})
+    return Results.Ok(podName);
+})
 .WithName("test");
+
+app.MapGet("/fibonacci", () =>
+{
+    int random = new Random().Next(1, 51); // Genera un número aleatorio entre 1 y 1000
+    // Simular un trabajo intensivo de CPU calculando un número grande de Fibonacci
+
+    int n = random; // Puedes ajustar este valor para controlar la intensidad del cálculo
+    BigInteger fibonacciResult = CalculateFibonacci(n);
+
+    string podName = Environment.GetEnvironmentVariable("HOSTNAME") ?? "unknown-pod";
+    return Results.Ok($"{podName} - Fibonacci({n}) = {fibonacciResult}");
+})
+.WithName("fibonacci");
+
+// Función para calcular el número de Fibonacci de forma recursiva (intensivo en CPU)
+static BigInteger CalculateFibonacci(int n)
+{
+    if (n <= 1)
+        return n;
+    return CalculateFibonacci(n - 1) + CalculateFibonacci(n - 2);
+}
 
 app.Run("http://*:80");
